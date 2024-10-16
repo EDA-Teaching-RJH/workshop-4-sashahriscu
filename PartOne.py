65c505db29149b1e102482e4c195771dce6e086b
Alexandra Hriscu <ah2148@kent.ac.uk>
2024-10-16 22:01:59 +0000

    completion

--- PartOne.py
+++ PartOne.py
@@ -1,1 +1,15 @@
+# define principal function
+def main():
+    camel_case = input("Please enter the name of the variable in camel case: ")
+    snake_case = ""
 
+    for char in camel_case:
+        if char.isupper():
+            snake_case += "_" + char.lower()
+# else part covers everything that is not uppercase (numbers etc)
+        else:
+            snake_case += char
+    print(snake_case)
+
+# call principal function
+main()
